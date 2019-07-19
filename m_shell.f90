bab7f7f90a417847d868cfe4ba11e6347bf1de2e
St Maxwell <33542937+St-Maxwell@users.noreply.github.com>
2019-07-19 16:55:25 +0800

    forgot...

--- m_shell.f90
+++ m_shell.f90
@@ -2,7 +2,9 @@ module m_shell
   use m_combination
   implicit none
   private
-  public :: shell, s_shell, p_shell, d_shell, f_shell, g_shell
+  public :: shell, s_shell, p_shell, &
+            d_shell, f_shell, g_shell, &
+            shell_factory
 
   type :: spin_orb
     integer :: mag_qn
@@ -20,7 +22,12 @@ module m_shell
     integer(kind=long) :: num_comb
     type(combination), dimension(:), allocatable :: comb
     contains
+      ! initialize spin orbitals
+      ! calculate the number of microstates
+      ! allocate ?_shell%comb
       procedure(initialize_shell), deferred :: initialize
+      ! find all possible microstates
+      ! calculate L and S of each microstate
       procedure(get_combination_shell), deferred :: get_combination
   end type
 
@@ -40,12 +47,7 @@ module m_shell
   type, extends(shell) :: s_shell
     ! no extra attribute
     contains
-      ! initialize spin orbitals
-      ! calculate the number of microstates
-      ! allocate ?_shell%comb
       procedure, public :: initialize => initialize_sub_s
-      ! find all possible microstates
-      ! calculate L and S of each microstate
       procedure, public :: get_combination => get_combination_s
   end type
     
@@ -74,6 +76,44 @@ module m_shell
   end type
 
   contains
+ 
+  function shell_factory(shell_type, num_occ)
+  ! return shell class object and initialize it
+    implicit none
+    class(shell), pointer :: shell_factory
+    character(len=1), intent(in) :: shell_type
+    integer, intent(in) :: num_occ
+    !-------------------------------------------------------
+    type(s_shell), pointer :: s_type => null()
+    type(p_shell), pointer :: p_type => null()
+    type(d_shell), pointer :: d_type => null()
+    type(f_shell), pointer :: f_type => null()
+    type(g_shell), pointer :: g_type => null()
+  
+    select case(shell_type)
+    case('s')
+      allocate(s_type)
+      shell_factory => s_type
+    case('p')
+      allocate(p_type)
+      shell_factory => p_type
+    case('d')
+      allocate(d_type)
+      shell_factory => d_type
+    case('f')
+      allocate(f_type)
+      shell_factory => f_type
+    case('g')
+      allocate(g_type)
+      shell_factory => g_type
+    case default
+      stop "Unexpected shell type argument"
+    end select
+
+    call shell_factory%initialize(num_occ)
+
+  end function
+
   subroutine initialize_sub_s(this, n_occ)
     implicit none
     class(s_shell), intent(inout) :: this
@@ -295,186 +335,185 @@ module m_spectra
   implicit none
   contains
 
-    subroutine print_table(table, L_set, S_set, uni_L, uni_S)
-      implicit none
-      integer, dimension(:,:), intent(in) :: table
-      integer, dimension(:), intent(in) :: L_set
-      real, dimension(:), intent(in) :: S_set
-      integer, intent(in) :: uni_L, uni_S
-      !-----------------------------------------------------
-      character(len=10) :: L_label
-      character(len=150) :: table_row
-      character(len=:), allocatable :: tab_row_fmt
-      character(len=5) :: repeat
-      character(len=150) :: bottom_line
-      character(len=150) :: S_label
-      character(len=:), allocatable :: S_label_fmt      
-      !-----------------------------------------------------
-      integer :: iter
+  subroutine print_table(table, L_set, S_set)
+    implicit none
+    integer, dimension(:,:), intent(in) :: table
+    integer, dimension(:), intent(in) :: L_set
+    real, dimension(:), intent(in) :: S_set
+    !-----------------------------------------------------
+    character(len=10) :: L_label
+    character(len=150) :: table_row
+    character(len=:), allocatable :: tab_row_fmt
+    character(len=5) :: repeat
+    character(len=150) :: bottom_line
+    character(len=150) :: S_label
+    character(len=:), allocatable :: S_label_fmt      
+    !-----------------------------------------------------
+    integer :: iter
 
-      ! there are uni_S repeats in a row
-      ! format ??I6
-      write(repeat,"(I2)") uni_S
-      tab_row_fmt = '(' // trim(adjustl(repeat)) // 'I6)'
-      ! format ??F6.1
-      S_label_fmt = '(' // trim(adjustl(repeat)) // 'F6.1)'
+    ! there are size(S_set) repeats in a row
+    ! format ??I6
+    write(repeat,"(I2)") size(S_set)
+    tab_row_fmt = '(' // trim(adjustl(repeat)) // 'I6)'
+    ! format ??F6.1
+    S_label_fmt = '(' // trim(adjustl(repeat)) // 'F6.1)'
 
-      write(*,*) " "
-      write(*,*) "  ML |"
-      write(*,*) "     |"
+    write(*,*) " "
+    write(*,*) "  ML |"
+    write(*,*) "     |"
 
-      do iter = 1, uni_L
-        write(L_label,"(I4,' |')") L_set(iter)
-        write(table_row,fmt=tab_row_fmt) table(:,iter)
-        write(*,*) trim(L_label) // trim(table_row)
+    do iter = 1, size(L_set)
+      write(L_label,"(I4,' |')") L_set(iter)
+      write(table_row,fmt=tab_row_fmt) table(:,iter)
+      write(*,*) trim(L_label) // trim(table_row)
+    end do
+
+    bottom_line = "     |"
+    do iter = 1, size(S_set)+1
+      bottom_line = trim(bottom_line) // "______"
+    end do
+    write(*,*) trim(bottom_line)
+
+    write(S_label,fmt=S_label_fmt) S_set(:size(S_set))
+    write(*,*) "      " // trim(S_label) // "    MS"
+
+  end subroutine
+
+  subroutine print_term_symbol(table, uni_L, uni_S)
+  ! reference: Journal of Chemical Education, 52(2), 1975:87-89
+  !       doi: 10.1021/ed052p87
+    implicit none
+    integer, dimension(:,:), intent(in) :: table
+    integer, intent(in) :: uni_L, uni_S
+    integer, dimension(:,:), allocatable :: table_out
+    integer :: z, w, repeat
+    character(len=10) :: symbol
+    character(len=10), dimension(50) :: list_symbol
+    integer :: num_symbol, remain, nrows
+    integer :: iter, jter, kter, lter
+
+    z = (uni_L+1) / 2
+    w = ceiling(uni_S/2.0)
+
+    allocate(table_out(z,w))
+
+    table_out = 0
+    do iter = 1, z
+      do jter = 1, w
+        table_out(iter, jter) = table(jter,iter)
       end do
+    end do
 
-      bottom_line = "     |"
-      do iter = 1, uni_S+1
-        bottom_line = trim(bottom_line) // "______"
-      end do
-      write(*,*) trim(bottom_line)
-
-      write(S_label,fmt=S_label_fmt) S_set(:uni_S)
-      write(*,*) "      " // trim(S_label) // "    MS"
-
-    end subroutine
-
-    subroutine print_term_symbol(table, uni_L, uni_S)
-    ! reference: Journal of Chemical Education, 52(2), 1975:87-89
-    !       doi: 10.1021/ed052p87
-      implicit none
-      integer, dimension(:,:), intent(in) :: table
-      integer, intent(in) :: uni_L, uni_S
-      integer, dimension(:,:), allocatable :: table_out
-      integer :: z, w, repeat
-      character(len=10) :: symbol
-      character(len=10), dimension(50) :: list_symbol
-      integer :: num_symbol, remain, nrows
-      integer :: iter, jter, kter, lter
-
-      z = (uni_L+1) / 2
-      w = ceiling(uni_S/2.0)
-
-      allocate(table_out(z,w))
-
-      table_out = 0
-      do iter = 1, z
+    num_symbol = 1
+    do iter = 1, z
+      do while (.not. all(table_out(iter,:) <= 0))
         do jter = 1, w
-          table_out(iter, jter) = table(jter,iter)
-        end do
-      end do
+          if (table_out(iter, jter) <= 0) then
+            cycle
+          else
+            repeat = table_out(iter, jter)
+            call output_terms(symbol, (z-iter), &
+                              uni_S, jter, repeat) 
 
-      num_symbol = 1
-      do iter = 1, z
-        do while (.not. all(table_out(iter,:) <= 0))
-          do jter = 1, w
-            if (table_out(iter, jter) <= 0) then
-              cycle
-            else
-              repeat = table_out(iter, jter)
-              call output_terms(symbol, (z-iter), &
-                                uni_S, jter, repeat) 
-
-              list_symbol(num_symbol) = symbol
-              num_symbol = num_symbol + 1
-              do kter = iter, z
-                do lter = jter, w
-                  table_out(kter, lter) = &
-                    table_out(kter, lter) - repeat
-                end do
+            list_symbol(num_symbol) = symbol
+            num_symbol = num_symbol + 1
+            do kter = iter, z
+              do lter = jter, w
+                table_out(kter, lter) = &
+                  table_out(kter, lter) - repeat
               end do
-            end if
-          end do
+            end do
+          end if
         end do
       end do
+    end do
 
-      write(*,*) 
-      write(*,*) "Term Symbols:"
-      remain = mod(num_symbol-1, 5)
-      nrows = (num_symbol - remain - 1) / 5
-      if (nrows /= 0) then
-        do iter = 1, nrows
-          jter = 5 * iter - 4
-          kter = 5 * iter
-          write(*,*) list_symbol(jter:kter)
-        end do
-        write(*,*) list_symbol(kter+1:kter+remain)
-      else
-        write(*,*) list_symbol(:remain)
-      end if
+    write(*,*) 
+    write(*,*) "Term Symbols:"
+    remain = mod(num_symbol-1, 5)
+    nrows = (num_symbol - remain - 1) / 5
+    if (nrows /= 0) then
+      do iter = 1, nrows
+        jter = 5 * iter - 4
+        kter = 5 * iter
+        write(*,*) list_symbol(jter:kter)
+      end do
+      write(*,*) list_symbol(kter+1:kter+remain)
+    else
+      write(*,*) list_symbol(:remain)
+    end if
 
-    end subroutine
+  end subroutine
 
-    subroutine output_terms(symbol, L, S_num, seq, times)
-    ! term symbol: (2S+1)L<times>
-    ! 2S + 1 = multiplicity, but here we do not obtain it directly via S
-      implicit none
-      character(len=10) :: symbol
-      integer :: L, S_num, seq, times
-      integer :: multiplicity
-      character :: term
+  subroutine output_terms(symbol, L, S_num, seq, times)
+  ! term symbol: (2S+1)L<times>
+  ! 2S + 1 = multiplicity, but here we do not obtain it directly via S
+    implicit none
+    character(len=10) :: symbol
+    integer :: L, S_num, seq, times
+    integer :: multiplicity
+    character :: term
 
-      multiplicity = S_num - 2*seq + 2
+    multiplicity = S_num - 2*seq + 2
 
-      select case(L)
-      case(0)
-        term = "S"
-      case(1)
-        term = "P"
-      case(2)
-        term = "D"
-      case(3)
-        term = "F"        
-      case(4)
-        term = "G"
-      case(5)
-        term = "H"    
-      case(6)
-        term = "I"
-      case(7)
-        term = "K"    
-      case(8)
-        term = "L"
-      case(9)
-        term = "M"
-      case(10)
-        term = "N"
-      case(11)
-        term = "O"
-      case(12)
-        term = "Q"
-      case(13)
-        term = "R"        
-      case(14)
-        term = "T"
-      case(15)
-        term = "U"    
-      case(16)
-        term = "V"
-      case(17)
-        term = "W"    
-      case(18)
-        term = "X"
-      case(19)
-        term = "Y" 
-      case(20)
-        term = "Z"
-      case default
-        write(term,"(I2)") L
-      end select
+    select case(L)
+    case(0)
+      term = "S"
+    case(1)
+      term = "P"
+    case(2)
+      term = "D"
+    case(3)
+      term = "F"        
+    case(4)
+      term = "G"
+    case(5)
+      term = "H"    
+    case(6)
+      term = "I"
+    case(7)
+      term = "K"    
+    case(8)
+      term = "L"
+    case(9)
+      term = "M"
+    case(10)
+      term = "N"
+    case(11)
+      term = "O"
+    case(12)
+      term = "Q"
+    case(13)
+      term = "R"        
+    case(14)
+      term = "T"
+    case(15)
+      term = "U"    
+    case(16)
+      term = "V"
+    case(17)
+      term = "W"    
+    case(18)
+      term = "X"
+    case(19)
+      term = "Y" 
+    case(20)
+      term = "Z"
+    case default
+      write(term,"(I2)") L
+    end select
 
-      select case(times)
-      case(1)
-        write(symbol,"(I1,A1)") multiplicity, term
-      case(2:9)
-        write(symbol,"((I1,A1,'<',I1,'>'))") multiplicity, &
-                                               term, times
-      case(10:)
-        write(symbol,"((I1,A1,'<',I2,'>'))") multiplicity, &
-                                               term, times
-      end select
+    select case(times)
+    case(1)
+      write(symbol,"(I1,A1)") multiplicity, term
+    case(2:9)
+      write(symbol,"((I1,A1,'<',I1,'>'))") multiplicity, &
+                                             term, times
+    case(10:)
+      write(symbol,"((I1,A1,'<',I2,'>'))") multiplicity, &
+                                             term, times
+    end select
 
-    end subroutine
+  end subroutine
 
 end module
--- main.f90
+++ main.f90
@@ -13,11 +13,6 @@ program main
   type(link), pointer :: head => null()
   type(link), pointer :: tail => null()
   type(link), pointer :: ptr => null()
-  type(s_shell), pointer :: s_define_p => null()
-  type(p_shell), pointer :: p_define_p => null()
-  type(d_shell), pointer :: d_define_p => null()
-  type(f_shell), pointer :: f_define_p => null()
-  type(g_shell), pointer :: g_define_p => null()
   class(shell), pointer :: shell_ptr => null()
 !-----------------------------------------------------------
   integer(kind=long), dimension(:), allocatable :: shells_num_comb
@@ -29,22 +24,20 @@ program main
   real, dimension(:), allocatable :: S_all_comb_sorted
   integer, dimension(:), allocatable :: L_shell_comb
   real, dimension(:), allocatable :: S_shell_comb
-  integer, dimension(100) :: L_set
-  integer :: unique_L
-  real, dimension(100) :: S_set
-  integer :: unique_S
+  integer, dimension(:), allocatable :: L_set
+  real, dimension(:), allocatable :: S_set
   integer, dimension(:,:), allocatable :: table
 !-----------------------------------------------------------
-  integer :: itrav
-  integer(kind=long) :: istep
+  integer :: jter
+  integer(kind=long) :: kter
   integer(kind=long) :: iter
-  integer(kind=long) :: icounter
+  integer(kind=long) :: lter
   integer :: iL, iS
 !-----------------------------------------------------------
   write(*,*) "------------------------------------------------------"
   write(*,*) "    This program aims for determining term symbols    "
   write(*,*) "               Programmed by St Maxwell               "
-  write(*,*) "                    2019-jul-16th                     "
+  write(*,*) "                    2019-Jul-19th                     "
   write(*,*) "------------------------------------------------------"
   write(*,*)
   write(*,*) "Input the number of shells:"
@@ -60,42 +53,13 @@ program main
   write(*,*) "Input occupation numbers for each shell (e.g. 1 3 1 2):"
   read(*,*) shell_occ
 
-  iter = 1
-  do while (.true.)
   ! build a linked list
   ! each link receives a shell
-    select case(shell_type(iter))
-    case('s')
-      allocate(s_define_p)
-      call s_define_p%initialize(shell_occ(iter))
-      shells_num_comb(iter) = s_define_p%num_comb
-      shell_ptr => s_define_p
-      s_define_p => null()
-    case('p')
-      allocate(p_define_p)
-      call p_define_p%initialize(shell_occ(iter))
-      shells_num_comb(iter) = p_define_p%num_comb
-      shell_ptr => p_define_p
-      p_define_p => null()
-    case('d')
-      allocate(d_define_p)
-      call d_define_p%initialize(shell_occ(iter))
-      shells_num_comb(iter) = d_define_p%num_comb
-      shell_ptr => d_define_p
-      d_define_p => null()
-    case('f')
-      allocate(f_define_p)
-      call f_define_p%initialize(shell_occ(iter))
-      shells_num_comb(iter) = f_define_p%num_comb
-      shell_ptr => f_define_p
-      f_define_p => null()
-    case('g')
-      allocate(g_define_p)
-      call g_define_p%initialize(shell_occ(iter))
-      shells_num_comb(iter) = g_define_p%num_comb
-      shell_ptr => g_define_p
-      g_define_p => null()
-    end select 
+  do iter = 1, num_shell
+  
+    ! creat shell object
+    shell_ptr => shell_factory(shell_type(iter), shell_occ(iter))
+    shells_num_comb(iter) = shell_ptr%num_comb
 
     if (.not. associated(head)) then
       allocate(head)
@@ -109,12 +73,10 @@ program main
       tail%variable => shell_ptr
     end if
 
-    if (iter == num_shell) exit
-    iter = iter + 1
-
   end do
 
   ! obtain the total number of microstates
+  ! and allocate working arrays
   tot_num_comb = 1
   do iter = 1, num_shell
     tot_num_comb = tot_num_comb * shells_num_comb(iter)
@@ -126,11 +88,10 @@ program main
 
   L_all_comb = 0
   S_all_comb = 0
-  itrav = 1
+  jter = 1
   ptr => head
 
-  do while (.true.)
-    if (.not. associated(ptr)) exit
+  do while ( associated(ptr) )
     ! find microstates
     shell_ptr => ptr%variable
 
@@ -139,28 +100,30 @@ program main
     S_shell_comb = shell_ptr%comb(:)%S
     
     ! combinate microstates of each shell
-    ! to get total microstates
-    istep = 1
-    do iter = itrav+1, num_shell
-      istep = istep * shells_num_comb(iter)
+    ! to get all total microstates
+    !-------------------------------------------------------
+    kter = 1
+    do iter = jter+1, num_shell
+      kter = kter * shells_num_comb(iter)
     end do
-    itrav = itrav + 1
+    jter = jter + 1
 
-    icounter = 1
-    do iter = 1, tot_num_comb, istep
+    lter = 1
+    do iter = 1, tot_num_comb, kter
 
-      L_all_comb(iter:iter+istep-1) = &
-        L_all_comb(iter:iter+istep-1) + L_shell_comb(icounter)
-      S_all_comb(iter:iter+istep-1) = &
-        S_all_comb(iter:iter+istep-1) + S_shell_comb(icounter)
+      L_all_comb(iter:iter+kter-1) = &
+        L_all_comb(iter:iter+kter-1) + L_shell_comb(lter)
+      S_all_comb(iter:iter+kter-1) = &
+        S_all_comb(iter:iter+kter-1) + S_shell_comb(lter)
 
-      if (icounter == size(L_shell_comb)) then
-        icounter = 1
+      if (lter == size(L_shell_comb)) then
+        lter = 1
         cycle
       end if
-      icounter = icounter + 1
+      lter = lter + 1
 
     end do
+    !-------------------------------------------------------
 
     ptr => ptr%next
 
@@ -171,27 +134,27 @@ program main
   S_all_comb_sorted = S_all_comb
   call sort_L(L_all_comb_sorted)
   call sort_S(S_all_comb_sorted)
-  call set_L(L_all_comb_sorted, L_set, unique_L)
-  call set_S(S_all_comb_sorted, S_set, unique_S)
+  L_set = set_of_L(L_all_comb_sorted)
+  S_set = set_of_S(S_all_comb_sorted)
   deallocate(L_all_comb_sorted)
   deallocate(S_all_comb_sorted)
 
   ! build the table
   ! reference: Journal of Chemical Education, 52(2), 1975:87-89
-  allocate(table(unique_S, unique_L))
+  allocate(table(size(S_set), size(L_set)))
   table = 0
   do iter = 1, tot_num_comb
-    do iL = 1, unique_L
+    do iL = 1, size(L_set)
       if (L_set(iL) == L_all_comb(iter)) exit
     end do
-    do iS = 1, unique_S
+    do iS = 1, size(S_set)
       if (abs(S_set(iS)-S_all_comb(iter)) < tol) exit
     end do
     table(iS, iL) = table(iS, iL) + 1
   end do
 
-  call print_table(table, L_set, S_set, unique_L, unique_S)
-  call print_term_symbol(table, unique_L, unique_S)
+  call print_table(table, L_set, S_set)
+  call print_term_symbol(table, size(L_set), size(S_set))
 
   write(*,*) "Finished. Press Enter to exit."
   read(*,*)
